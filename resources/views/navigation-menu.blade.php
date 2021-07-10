0abe757b563f59d47ff9cf60798b021ee74b90a4
Carlos <carlosceravargas@gmail.com>
2021-07-10 15:01:22 -0500

    vista tablero
    Traducccion

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -3,6 +3,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
+
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
@@ -10,11 +11,20 @@
                     </a>
                 </div>
 
+                
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Inicio') }}
                     </x-jet-nav-link>
+
+                    {{-- href="{{ route('incidencias') }}" --}}
+
+                    <x-jet-nav-link>
+                        {{ __('Incidencias Registradas') }}
+                    </x-jet-nav-link>
+
                 </div>
             </div>
 
@@ -140,6 +150,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Tablero') }}
+            </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('incidencias')">
+                {{ __('Incidencias Registradas') }}
             </x-jet-responsive-nav-link>
         </div>
 
